bd303764173c414bc3ce0a7ff3c9d8865bdb66e1
NastyaArt <artemchuk.nastya@gmail.com>
2018-05-03 13:50:32 +0300

    add tabs and tables

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,7 +3,8 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.event.*;
 import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import server.Equipment;
 import server.Cabinet;
 import server.Responsible;
@@ -16,6 +17,12 @@ public class Controller {
     public Button btnAdd;
     public Button btnDel;
     public Button btnEdit;
+    public TableView<Equipment> tableEquipment;
+    public TableView<Cabinet> tableCabinet;
+    public TableView<Responsible> tableResponsible;
+    public TableView<Distribution> tableDistribution;
+    public TableView<Writeoff> tableWriteoff;
+
     private Client client;
 
     @FXML
@@ -27,11 +34,22 @@ public class Controller {
         List<Responsible> listResponsible = client.getResponsible();
         List<Distribution> listDistribution = client.getDistribution();
         List<Writeoff> listWriteoff = client.getWriteoff();
+
+        ObservableList<Equipment> dataEquipment = FXCollections.observableArrayList(listEquipment);
+        tableEquipment.setItems(dataEquipment);
+        ObservableList<Cabinet> dataCabinet = FXCollections.observableArrayList(listCabinet);
+        tableCabinet.setItems(dataCabinet);
+        ObservableList<Responsible> dataResponsible = FXCollections.observableArrayList(listResponsible);
+        tableResponsible.setItems(dataResponsible);
+        ObservableList<Distribution> dataDistribution = FXCollections.observableArrayList(listDistribution);
+        tableDistribution.setItems(dataDistribution);
+        ObservableList<Writeoff> dataWriteoff = FXCollections.observableArrayList(listWriteoff);
+        tableWriteoff.setItems(dataWriteoff);
+
     }
 
     public void Add(ActionEvent actionEvent) {
-        List<Equipment> list = client.getEquipment();
-        btnAdd.setText(list.get(1).getName());
+        
     }
 
     public void Delete(ActionEvent actionEvent) {
